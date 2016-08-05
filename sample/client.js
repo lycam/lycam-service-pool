4e90c771be173cbf083455d65b0d85542e0a97de
no777 <wt@lycam.tv>
2016-08-05 17:16:15 +0800

    no message

--- sample/client.js
+++ sample/client.js
@@ -5,8 +5,22 @@ var pjson = require('../package.json');
 var pool = new Pool({
   etcdHosts: process.env.ETCD_HOSTS ||  'localhost',
   appName: pjson.name,
+  enableEndpointUpdate:true
 });
 // console.log(pool);
+
+var SrviceRegistry = pool.serviceRegistry;
+
+SrviceRegistry.on('endpoint_add',function(err,data){
+  console.log("endpoint_add",err,data);
+});
+SrviceRegistry.on('endpoint_update',function(err,data){
+  console.log("endpoint_update",err,data);
+});
+SrviceRegistry.on('endpoint_remove',function(err,data){
+  console.log("endpoint_remove",err,data);
+});
+
 pool.createClient(EchoService)
 .then(function (client) {
   // console.log(client);
